1cd4072363eb51adce3934797a4c2066f2d05a93
Cpt_Crunch <jasong84@gmail.com>
2020-10-11 18:37:44 -0500

    Finalize style of Header component

--- src/components/header.js
+++ src/components/header.js
@@ -6,30 +6,30 @@ import {
   NavbarToggler
 } from 'reactstrap';
 import Link from './link'
-import Navigation from 'reactstrap-json-nav'
-import navigation from '../data/navigation';
+import pageLinks from '../data/navigation';
 import logo from '../assets/images/logo_ap2.png'
 import styled from 'styled-components'
+import Navigation from 'reactstrap-json-nav'
 
-const StyledNavbar = styled(props => <Navbar {...props} />)`
-  background: gold;
+
+const StyledNavbar = styled(Navbar)`
+  border-bottom: 8px solid rgb(64,75,137);
 `
-
 
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <StyledNavbar expand="md">
+    <StyledNavbar color="light" light expand="md">
       <Container>
         <Link to="/" className="navbar-brand">
-          <img src={logo} alt={props.siteTitle} height="45px"/>
+          <img src={logo} alt={props.siteTitle} height="90px"/>
         </Link>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Navigation
-            json={navigation}
+            json={pageLinks}
             link={Link}
             activeClassName="active"
           />
